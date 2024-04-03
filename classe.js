c50368e3d5740093435fb23382ca0f720585165d
PEDRO MAMEDES BRAGHINE DA SILVA <pedro_mb_silva@estudante.sesisenai.org.br>
2024-04-03 08:06:23 -0300

    Adicionado clasObj e pasta assets

--- classe.js
+++ classe.js
@@ -1,13 +1,21 @@
 class Obj{
 
-   
+    set_visible = true
   
     constructor(x,y,w,h, image){
-      
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.image = image
     }
   
     draw(){
-     
+        if (this.set_visible) {
+            var img = new Image()
+            img.src = this.image
+            canvas.drawImage(img, this.x, this.y, this.w, this.h)
+          }
     }
   
     collide(obj){
